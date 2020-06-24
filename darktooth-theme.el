af738c0f74e5339d8d462f7dbc6f04d1b3e72766
Troy Hinckley <t.macman@gmail.com>
2020-06-24 09:28:50 -0700

    Fix warning color

--- darktooth-theme.el
+++ darktooth-theme.el
@@ -159,7 +159,7 @@
   (compilation-mode-line-fail                (:foreground darktooth-neutral_red))
   (error                                     (:foreground darktooth-bright_red :bold t))
   (success                                   (:foreground darktooth-neutral_green :bold t))
-  (warning                                   (:foreground darktooth-bright_yellow :bold t))
+  (warning                                   (:foreground darktooth-bright_orange :bold t))
 
   ;; Built-in syntax
   (font-lock-builtin-face                            (:foreground darktooth-bright_orange))
